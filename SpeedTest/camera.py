63025563fd192f78729871c91dcf7d102c226f7f
dtokazaki <daniel.t.okazaki@gmail.com>
2019-04-02 15:21:00 -0700

    New NCS power test, along with GPIO cleanup program. Also includes speed tests for the USB cam and pi cam

--- SpeedTest/camera.py
+++ SpeedTest/camera.py
@@ -3,12 +3,11 @@ import time
 
 camera = PiCamera()
 avg = 0
-for i in range(0,10):
+for i in range(0,50):
     start = time.time()
 
     camera.start_preview()
-    #time.sleep(2)
-    camera.capture('/home/pi/Desktop/SpeedTest/image.jpg')
+    camera.capture('/home/pi/Desktop/NavSense/SpeedTest/image.jpg')
     camera.stop_preview()
 
     end = time.time()
--- SpeedTest/usbcamera.py
+++ SpeedTest/usbcamera.py
@@ -1,13 +1,14 @@
 import cv2
 import time
 
-video_capture = cv2.VideoCapture(0)
-# Check success
-if not video_capture.isOpened():
-        raise Exception("Could not open video device")
-# Read picture. ret === True on success
 avg = 0
-for i in range(0,10):
+for i in range(0,50):
+    video_capture = cv2.VideoCapture(0)
+    # Check success
+    if not video_capture.isOpened():
+            raise Exception("Could not open video device")
+    # Read picture. ret === True on success
+
     start = time.time()
     ret, frame = video_capture.read()
 
@@ -19,9 +20,9 @@ for i in range(0,10):
 #        break
 # Close device
     cv2.imwrite('image.png', frame)
-    #video_capture.release()
+    video_capture.release()
     end = time.time()
     avg = avg + (end-start)
 
-video_capture.release()
+    #video_capture.release()
 print(avg/i)
