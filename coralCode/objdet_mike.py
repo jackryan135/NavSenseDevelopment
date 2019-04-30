15f48b702597ae66af203359cf4c2a7409124f0e
Jack Ryan <jackryan135@gmail.com>
2019-04-30 16:58:57 -0700

    added tts

--- coralCode/objdet_mike.py
+++ coralCode/objdet_mike.py
@@ -19,12 +19,68 @@ import RPi.GPIO as GPIO
 from edgetpu.detection.engine import DetectionEngine
 from PIL import Image
 from threading import Thread, Lock
+from espeak import espeak
 
 button_mutex = Lock()
 ttx_mutex = Lock()
 interrupt = 0
 # Exit ttx function when set to 1
 stop_ttx = 0
+
+def multiples(dictionary, arr):
+    st = ''
+    if len(arr) != 0:
+        for key, value in arr.items():
+            if value == 1:
+                s = str(key)
+                st += 'one ' + dictionary[s] + ', '
+            if value <= 5 and value > 1:
+                st += str(value) + ' '
+                if key != 1:
+                    s = str(key)
+                    st += dictionary[s] + 's, '
+                else:
+                    st += 'people, '
+            if value > 5:
+                st += 'several '
+                if key != 1:
+                    s = str(key)
+                    st += dictionary[s] + 's, '
+                else:
+                    st += 'people, '
+    else:
+        st = 'Nothing '
+    return st
+
+def countItems(dictionary, arr):
+    counter = collections.Counter(arr)
+    c = dict(counter)
+    str = multiples(dictionary, c)
+    return str
+
+def parseObjects(obs):
+    left = []
+    center = []
+    right = []
+    # Parse Objects
+    for o in objs:
+        if o[1] < 100 and o[3] < 100:
+            left.append(o[0])
+        #if it creeps into center mark it as center
+        elif (o[1] >= 100 and o[1] <= 200) or (o[3] >= 100 and o[3] <= 200):
+            center.append(o[0])
+        elif o[1] > 200 and o[3] < 300:
+            right.append(o[0])
+    return left, center, right
+
+def constructString(dictionary, objs):
+    string = 'There is '
+    left, center, right = parseObjects(objs)
+    lStr = countItems(dictionary, left) + 'to your left. '
+    cStr = countItems(dictionary, center) + 'straight ahead. '
+    rStr = countItems(dictionary, right) + 'to your right.'
+    string += lStr + cStr + 'And ' + rStr
+    return string
 
 # Function to read labels from text files.
 def ReadLabelFile(file_path):
@@ -47,16 +103,19 @@ def hardware_interrupt():
       while time.time() < stop:
         if GPIO.event_detected(3):
          GPIO.cleanup()
-         call("sudo shutdown -h now") 
+         call("sudo shutdown -h now")
       button_mutex.acquire()
-      interrupt = 1 
+      interrupt = 1
       button_mutex.release()
 
 def text_to_speech(result,labels):
   # Jack's Code
   print("Running Text To Speech")
+  string = constructString(labels, result)
+  espeak.synth(string)
 
 def main():
+  espeak.synth('Welcome to NavSense')
   # Parse Arguments
   parser = argparse.ArgumentParser()
   parser.add_argument(
@@ -66,6 +125,7 @@ def main():
   args = parser.parse_args()
 
   # Initialize engine.
+  espeak.synth('Loading Object Recognition Models')
   engine = DetectionEngine(args.model)
   labels = ReadLabelFile(args.label) if args.label else None
   result = None
@@ -75,29 +135,29 @@ def main():
 
   # Initialize Hardware Interrupt
   button_t.start()
-  
+
   while True:
     camera.capture('/home/pi/NavSense/coralCode/image.jpg')
     image = Image.open('image.jpg')
-    
+
     result = engine.DetectWithImage(image, threshold = 0.25, keep_aspect_ratio = True, relative_coord = False, top_k = 5)
     if result:
       # Start thread to run text to speech, when done, quit thread
-      text_to_speech(result, labels) 
+      text_to_speech(result, labels)
 
     # Sleep and check for hardware interrupt code
     start_ms = time.time()
-    while True:	
+    while True:
       print("loop")
       time.sleep(0.25)
       elapsed_ms = time.time() - start_ms
       button_mutex.acquire()
       if interrupt == 1:
-        interupt = 0 
+        interupt = 0
         button_mutex.release()
         break
-      
-      button_mutex.release() 
+
+      button_mutex.release()
       if elapsed_ms > 5:
         break
 
