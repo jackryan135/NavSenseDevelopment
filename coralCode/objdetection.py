1ba2e83059dcf2a323c22eb0e1a9e7522f4ebc06
dtokazaki <25940882+dtokazaki@users.noreply.github.com>
2019-04-30 15:05:36 -0700

    Update objdetection.py

--- coralCode/objdetection.py
+++ coralCode/objdetection.py
@@ -46,16 +46,12 @@ def hardware_interrupt(ttx_t):
          GPIO.cleanup()
          call("sudo shutdown -h now") 
       button_mutex.acquire()
-      try:
-        interrupt = 1 
-      finally:
-        button_mutex.release()
+      interrupt = 1 
+      button_mutex.release()
       if ttx_t.isAlive():
         ttx_mutex.acquire()
-        try:
-          stop_ttx = 1 
-        finally:
-          ttx_mutex.release()
+        stop_ttx = 1 
+        ttx_mutex.release()
 
 def text_to_speech(result,labels):
   # Jack's Code
@@ -84,33 +80,30 @@ def main():
     camera.resolution = (640, 480)
     camera.framerate = 30
     _, width, height, channels = engine.get_input_tensor_shape()
-    try:
-      stream = io.BytesIO()
-      while True:
-        for foo in camera.capture(stream,format='rgb', use_video_port=True, resize=(width, height)): 
-          stream.truncate()
-          stream.seek(0)
-          input = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-          result = engine.DetectWithImage(input, threshold = 0.25, keep_aspect_ratio = True, relative_coord = False, top_k = 5)
-          if results:
-            # Start thread to run text to speech, when done, quit thread
-            call_text_to_speech.start(result,args)
+    stream = io.BytesIO()
+  while True:
+    for foo in camera.capture(stream,format='rgb', use_video_port=True, resize=(width, height)): 
+      stream.truncate()
+      stream.seek(0)
+      input = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+      result = engine.DetectWithImage(input, threshold = 0.25, keep_aspect_ratio = True, relative_coord = False, top_k = 5)
+      if results:
+        # Start thread to run text to speech, when done, quit thread
+        call_text_to_speech.start(result,args)
 
-        # Sleep and check for hardware interrupt code
-        start_ms = time.time()
-        while True:	
-          time.sleep(0.25)
-          elapsed_ms = time.time() - start_ms
-          button_mutex.acquire()
-          try:
-            if interrupt == 1:
-              interupt = 0 
-              button_mutex.release()
-              break
-          finally:
-            button_mutex.release() 
-          if elapsed_ms > 50000:
-            break
+      # Sleep and check for hardware interrupt code
+      start_ms = time.time()
+      while True:	
+        time.sleep(0.25)
+        elapsed_ms = time.time() - start_ms
+        button_mutex.acquire()
+        if interrupt == 1:
+          interupt = 0 
+          button_mutex.release()
+          break
+        button_mutex.release() 
+        if elapsed_ms > 50000:
+          break
 
 if __name__ == '__main__':
   main()
