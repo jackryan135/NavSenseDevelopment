46c0fa785177fb4b10e01d03c4920d1d40984c0d
Jack Ryan <jackryan135@gmail.com>
2019-06-01 13:29:15 -0700

    button tester program

---
+++ coralCode/buttonWork.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+
+# Button interrupt function
+def hardware_interrupt(channel):
+    print("inference button was pressed")
+
+
+def button_up(channel):
+    print("UP")
+    if GPIO.input(13):
+        print("Volume up")
+    elif GPIO.input(15):
+        print("speeking speed up")
+
+
+def button_down(channel):
+    print("DOWN")
+    if not GPIO.input(13):
+        print("volume down")
+    if not GPIO.input(15):
+        print("speeking speed down")
+
+
+def power_off(channel):
+    print("OFF")
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Start of button interrupt
+GPIO.add_event_detect(
+    3, GPIO.FALLING, callback=hardware_interrupt, bouncetime=300)
+# Button up
+GPIO.add_event_detect(
+    11, GPIO.FALLING, callback=button_up, bouncetime=300)
+# Button down
+GPIO.add_event_detect(
+    29, GPIO.FALLING, callback=button_down, bouncetime=300)
+# Power off
+GPIO.add_event_detect(
+    32, GPIO.FALLING, callback=power_off, bouncetime=300)
+# Switch up: 35
+# Switch down: 38
+
+while(True)
