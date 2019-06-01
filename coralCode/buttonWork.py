18243627ea7caf4a380b48afcf9f2763d0295cb6
Jack Ryan <jackryan135@gmail.com>
2019-06-01 14:04:38 -0700

    PUD_UP

--- coralCode/buttonWork.py
+++ coralCode/buttonWork.py
@@ -32,10 +32,10 @@ def power_off(channel):
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Start of button interrupt
